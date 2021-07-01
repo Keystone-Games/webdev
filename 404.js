a2fef5b03316f13c59547266ae799b2413d149da
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-07-01 12:47:28 -0400

    add stuff

--- 404.js
+++
@@ -1,9 +0,0 @@
-function t_h1_kgn() 
-{
-    window.location.href = ".";
-}
-
-function b_sg() 
-{ 
-    window.location.href = "./games";
-}
---
+++ global.js
@@ -0,0 +1,9 @@
+function t_h1_kgn() 
+{
+    window.location.href = ".";
+}
+
+function b_sg() 
+{ 
+    window.location.href = "./games";
+}
