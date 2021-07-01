c12f6df5d42255657615e8c06a043b778fd8343c
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-07-01 14:54:05 -0400

    add stuff

--- games/games.js
+++ games/games.js
@@ -7,3 +7,13 @@ function b_soundspace_d()
 {
     window.location.href = "../soundspace/download/";
 }
+
+function b_blockate_i()
+{
+    window.location.href = "../blockate/";
+}
+
+function b_blockate_d()
+{
+    window.location.href = "../blockate/download/";
+}
