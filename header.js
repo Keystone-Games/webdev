6c83daf687cacd9909bdecac20a069107701d09f
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-09-04 11:33:45 -0400

    Update header.js

---
+++ header.js
@@ -0,0 +1,9 @@
+function t_h1_kgn() 
+{
+    window.location.href = "https://keystone-games.github.io";
+}
+
+function b_sg() 
+{ 
+    window.location.href = "https://keystone-games.github.io/games";
+}
