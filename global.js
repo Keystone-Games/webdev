05e5bd1cf146c6443fe3793562577ebc50dc3678
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-09-04 10:55:56 -0400

    changes

--- global.js
+++ global.js
@@ -1,9 +1,9 @@
 function t_h1_kgn() 
 {
-    window.location.href = ".";
+    window.location.href = "https://keystone-games.github.io";
 }
 
 function b_sg() 
 { 
-    window.location.href = "./games";
+    window.location.href = "https://keystone-games.github.io/games";
 }
