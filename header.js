18a100a96a93bccda89550bff12c59651efc2c7b
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-09-04 11:35:41 -0400

    Update header.js

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 function t_h1_kgn() 
 {
-    window.location.href = "https://keystone-games.github.io";
+    top.window.location.href = "https://keystone-games.github.io";
 }
 
 function b_sg() 
 { 
-    window.location.href = "https://keystone-games.github.io/games";
+    top.window.location.href = "https://keystone-games.github.io/games";
 }
