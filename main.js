8cbff6905b3a7b238da38715641413a7f1c09f58
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-09-21 12:08:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 function goto_github() { window.location.href = "https://github.com/keystone-games/"; }
+function goto_process() { window.location.href = "devprocess/"; }
+function goto_ltc() { window.location.href = ""; }
